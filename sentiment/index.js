29fa835009a0a1c03b3c7c3992feb4dc1081ca8f
mikebashford <mikebashford@gmail.com>
2024-02-28 17:38:48 -0500

    Fixed some typos

--- sentiment/index.js
+++ sentiment/index.js
@@ -18,7 +18,7 @@ app.use(expressPino);
 app.post('/sentiment', async (req, res) => {
 
     // Task 4: extract the sentence parameter
-    const { sentence } = req.query.sentence;
+    const { sentence } = req.query;
 
 
     if (!sentence) {
@@ -56,7 +56,7 @@ app.post('/sentiment', async (req, res) => {
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-        res.status(500).json({'message': 'Error performing sentiment analysisetching gift'});
+        res.status(500).json({'message': 'Error performing sentiment analysis'});
     }
 });
 
