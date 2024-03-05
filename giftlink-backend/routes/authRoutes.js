66dd0e4744f05bc2e61c84c646d9461edd511531
mikebashford <mikebashford@gmail.com>
2024-03-04 20:21:39 -0500

    Implemented update user route

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -8,8 +8,8 @@ const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  
 
+const { body, validationResult } = require('express-validator');
 const logger = pino(); 
-
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -72,6 +72,46 @@ router.post('/login', async (req, res) => {
     } catch (e) {
          return res.status(500).json({ error: 'Internal server error', details: e.message });
     }
+});
+
+router.put('/update', async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+
+        const existingUser = await collection.findOne({ email });
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "User not found" });
+        }
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+            
+        );
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        res.json({authtoken});
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+}
 });
 
 module.exports = router;
